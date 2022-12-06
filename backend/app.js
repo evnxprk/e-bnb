93090787e9dfff4dc44461c135f0bc4988ed73ac
Eunice Park <eunicexpark01@gmail.com>
2022-12-06 09:14:50 -0800

    phase 2 error handling

--- backend/app.js
+++ backend/app.js
@@ -10,6 +10,7 @@ const isProduction = environment === "production";
 const routes = require("./routes");
 const router = require("./routes");
 const app = express();
+const { ValidationError } = require("sequelize");
 
 app.use(morgan("dev"));
 app.use(cookieParser());
@@ -38,6 +39,37 @@ app.use(
     }
   })
 );
+
+// backend/app.js
+// ...
+// Catch unhandled requests and forward to error handler.
+app.use((_req, _res, next) => {
+  const err = new Error("The requested resource couldn't be found.");
+  err.title = "Resource Not Found";
+  err.errors = ["The requested resource couldn't be found."];
+  err.status = 404;
+  next(err);
+});
+
+app.use((err, _req, _res, next) => {
+  // check if error is a Sequelize error:
+  if (err instanceof ValidationError) {
+    err.errors = err.errors.map((e) => e.message);
+    err.title = "Validation error";
+  }
+  next(err);
+});
+
+app.use((err, _req, res, _next) => {
+  res.status(err.status || 500);
+  console.error(err);
+  res.json({
+    title: err.title || "Server Error",
+    message: err.message,
+    errors: err.errors,
+    stack: isProduction ? null : err.stack,
+  });
+});
 
 app.use(routes);
 
