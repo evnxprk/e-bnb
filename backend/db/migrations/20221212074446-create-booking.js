471df5b986851ab5809b2df70c7f86c25b9b4f3d
Eunice Park <eunicexpark01@gmail.com>
2022-12-12 13:16:31 -0800

    update migrations and seeding

--- backend/db/migrations/20221212074446-create-booking.js
+++ backend/db/migrations/20221212074446-create-booking.js
@@ -5,8 +5,6 @@ if (process.env.NODE_ENV === "production") {
   options.schema = process.env.SCHEMA; // define your schema in options object
 }
 
-const { sequelize } = require('../models');
-
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up(queryInterface, Sequelize) {
