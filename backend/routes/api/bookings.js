969a6147c282148a463dc97e222716a17a52621c
Eunice Park <eunicexpark01@gmail.com>
2022-12-23 16:49:00 -0800

    update past modification

--- backend/routes/api/bookings.js
+++ backend/routes/api/bookings.js
@@ -42,7 +42,7 @@ router.put("/:bookingId", requireAuth, async (req, res, next) => {
       },
     });
 }
-if(endDate <= new Date ().endDate) {
+if(myBooking.endDate.getTime() < (new Date ()).getTime()) {
         res.status(403)
     res.json({
         message: "Past bookings cannot be modified",
